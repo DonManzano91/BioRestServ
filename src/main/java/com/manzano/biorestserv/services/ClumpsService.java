a663fced751ea2c5e9dcace285bbdae207259cd8
DonManzano91 <alex.batt99@gmail.com>
2025-01-28 14:14:23 -0600

    Ajustando Test a clump service

--- src/main/java/com/manzano/biorestserv/services/ClumpsService.java
+++ src/main/java/com/manzano/biorestserv/services/ClumpsService.java
@@ -6,7 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.HashSet;
+    import java.util.Map;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
--- src/test/java/com/manzano/biorestserv/ServiceTest/ClumpsServiceTest.java
+++ src/test/java/com/manzano/biorestserv/ServiceTest/ClumpsServiceTest.java
@@ -19,11 +19,10 @@ import java.util.Set;
 public class ClumpsServiceTest {
 
     private ClumpsService clumpsService;
-    private Utility utility;
 
     @BeforeEach
     public void setUp(){
-        utility = new Utility();
+        Utility utility = new Utility();
         clumpsService = new ClumpsService(utility);
 
     }
@@ -38,6 +37,41 @@ public class ClumpsServiceTest {
         Set<String> expectedOutput =new HashSet<>(Arrays.asList("GAAGA", "CGACA"));
         Assertions.assertEquals(expectedOutput, resolvedOutput);
     }
+
+    @Test
+    public void clumpFindingSuccess2(){
+        String gen = "CTAAAACGTCG";
+        int kmerLenght = 2;
+        int LongOfWindow = 4;
+        int timesKmerRepeats = 2;
+        Set<String> resolvedOutput = clumpsService.clumpFinding(gen, kmerLenght, LongOfWindow, timesKmerRepeats);
+        Set<String> expectedOutput =new HashSet<>(Arrays.asList("AA"));
+        Assertions.assertEquals(expectedOutput, resolvedOutput);
+    }
+
+    @Test
+    public void clumpFindingSuccess3(){
+        String gen = "ACGTACGT";
+        int kmerLenght = 1;
+        int LongOfWindow = 5;
+        int timesKmerRepeats = 2;
+        Set<String> resolvedOutput = clumpsService.clumpFinding(gen, kmerLenght, LongOfWindow, timesKmerRepeats);
+        Set<String> expectedOutput =new HashSet<>(Arrays.asList("T", "C", "A", "G"));
+        Assertions.assertEquals(expectedOutput, resolvedOutput);
+    }
+
+    @Test
+    public void clumpFindingSuccess4(){
+        String gen = "CCATATACC";
+        int kmerLenght = 3;
+        int LongOfWindow = 5;
+        int timesKmerRepeats = 2;
+        Set<String> resolvedOutput = clumpsService.clumpFinding(gen, kmerLenght, LongOfWindow, timesKmerRepeats);
+        Set<String> expectedOutput =new HashSet<>(Arrays.asList("ATA"));
+    Assertions.assertEquals(expectedOutput, resolvedOutput);
+    }
+
+
 
 
 }
